6a267d1ab3493f2f757ef17720802c1240293878
Yao <apollord.yao@gmail.com>
2020-10-19 14:47:10 +0800

    調整

--- test/cash/login.py
+++ test/cash/login.py
@@ -10,23 +10,23 @@ browser = webdriver.Chrome(options=options)
 browser.maximize_window()
 
 # 開啟頁面
-URL = "http://www.yaotest.orz"
-browser.get(URL + "/indexs")
+URL = "http://localhost:8080/"
+browser.get(URL)
 time.sleep(1)
 
-# 輸入帳號(博霸版本，可能依據不同版面抓的css selector不同)
-inputElement = browser.find_element_by_id("txtUser")
+# 輸入帳號
+inputElement = browser.find_element_by_css_selector("#LoginArea input[name='Account']")
 inputElement.send_keys("yaon4")
 time.sleep(0.5)
 
-# 輸入密碼(博霸版本，可能依據不同版面抓的css selector不同)
-inputElement = browser.find_element_by_id("txtPassword")
+# 輸入密碼
+inputElement = browser.find_element_by_css_selector("#LoginArea input[name='Password']")
 inputElement.send_keys("123456")
 time.sleep(0.5)
 
 # 驗證碼處理
 # 抓取 backend/captcha/{SR}
-SR = int(browser.find_element_by_css_selector("img.captcha_img").get_attribute('src').replace(URL, "").split('/')[3])
+SR = int(browser.find_element_by_css_selector("#LoginArea .VerificationFrame > img").get_attribute('src').replace(URL, "").split('/')[2])
 pCodeArray = [9697, 3673, 4549, 2671, 5981, 3911, 2243, 9479, 9743, 5981, 4999, 7411, 3259, 7211, 4397, 1789, 4139,
               8923, 8009, 7321, 1973, 7687, 1723, 4253, 7883, 4789, 9049, 3847, 9769, 1511, 1871]
 qCodeArray = [8387, 3919, 1279, 5653, 6569, 8297, 6043, 2141, 2341, 2741, 4129, 6967, 3617, 8731, 3947, 9203, 8269,
@@ -38,12 +38,12 @@ randCode = int(SR / (pCodeM * qCodeM))
 checkCode = int(randCode * (pCodeM + 1) / 1000)
 
 # 輸入驗證碼
-inputElement = browser.find_element_by_id("rmNum")
+inputElement = browser.find_element_by_css_selector("#LoginArea input[name='VerificationCode']")
 inputElement.send_keys(checkCode)
 time.sleep(0.5)
 
 # 登入
-browser.find_element_by_css_selector(".btn_login").click()
+browser.find_element_by_id("LoginArea").submit()
 time.sleep(5)
 
 browser.quit()
